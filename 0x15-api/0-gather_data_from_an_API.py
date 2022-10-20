a50b34709bedca3f50a102c394939aa7aadf0679
Stephen Nwankwo <nwankwostephen039@yahoo.com>
2022-10-20 01:57:46 +0100

    fix 0-1

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,45 +1,22 @@
 #!/usr/bin/python3
-"""A Python script that, using a REST API,
-for a given employee ID,returns information about
-his/her todo list progress
 """
+Uses https://jsonplaceholder.typicode.com along with an employee ID to
+return information about the employee's todo list progress
+"""
+
 import requests
 from sys import argv
 
-
 if __name__ == '__main__':
-    """Query https://jsonplaceholder.typicode.com for info"""
-    # Get user ID from STDIN
-    args = argv
-    u_id = args[1]
-
-    # Making a GET request
-    req_1 = requests.get(
-        'https://jsonplaceholder.typicode.com/users/{0}/todos'
-        .format(u_id))
-    req_2 = requests.get(
-        'https://jsonplaceholder.typicode.com/users/{0}'
-        .format(u_id))
-    req_3 = requests.get(
-        'https://jsonplaceholder.typicode.com/users/{0}/todos?completed=true'
-        .format(u_id))
-
-    # Extract data in json format
-    u_todo = req_1.json()
-    u_info = req_2.json()
-    done_task = req_3.json()
-
-    # Get total no of todo
-    todo_len = len(u_todo)
-
-    # Get total no of done todo
-    done_todo = len(done_task)
-
-    # print(done_task[0]['title'])
-    # Output info
-    print(
-        'Employee {0} is done with tasks ({1}/{2}):'
-        .format(u_info['name'], done_todo, todo_len))
-
-    for todo in range(len(done_task)):
-        print('\t {0}'.format(done_task[todo]['title']))
+    userId = argv[1]
+    user = requests.get("https://jsonplaceholder.typicode.com/users/{}".
+                        format(userId), verify=False).json()
+    todo = requests.get("https://jsonplaceholder.typicode.com/todos?userId={}".
+                        format(userId), verify=False).json()
+    completed_tasks = []
+    for task in todo:
+        if task.get('completed') is True:
+            completed_tasks.append(task.get('title'))
+    print("Employee {} is done with tasks({}/{}):".
+          format(user.get('name'), len(completed_tasks), len(todo)))
+    print("\n".join("\t {}".format(task) for task in completed_tasks))
--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,43 +1,22 @@
 #!/usr/bin/python3
-"""A Python script that, using a REST API,
-for a given employee ID,returns information about
-his/her todo list progress
 """
+Uses https://jsonplaceholder.typicode.com along with an employee ID to
+return information about the employee's todo list progress
+"""
+
+import csv
 import requests
 from sys import argv
 
-
 if __name__ == '__main__':
-    """Query https://jsonplaceholder.typicode.com for info"""
-    # Get user ID from STDIN
-    args = argv
-    u_id = args[1]
-
-    # Making a GET request
-    req_1 = requests.get(
-        'https://jsonplaceholder.typicode.com/users/{0}/todos'
-        .format(u_id))
-    req_2 = requests.get(
-        'https://jsonplaceholder.typicode.com/users/{0}'
-        .format(u_id))
-
-    # Extract data
-    u_todo = req_1.json()
-    u_info = req_2.json()
-    USER_ID = u_info['id']
-    USERNAME = u_info['username']
-
-    # Get total no of done todo
-    todo_len = len(u_todo)
-
-    # Save content to csv file
-    with open(f'{USER_ID}.csv', 'w') as f:
-        for todo in range(todo_len):
-            TASK_TITLE = u_todo[todo]['title']
-            TASK_COMPLETED_STATUS = u_todo[todo]['completed']
-            TASK_TITLE = u_todo[todo]['title']
-            str_1 = f'"{USER_ID}","{USERNAME}",'
-            str_2 = f'"{TASK_COMPLETED_STATUS}","{TASK_TITLE}"'
-            line = str_1 + str_2
-            f.write(line)
-            f.write('\n')
+    userId = argv[1]
+    user = requests.get("https://jsonplaceholder.typicode.com/users/{}".
+                        format(userId), verify=False).json()
+    todo = requests.get("https://jsonplaceholder.typicode.com/todos?userId={}".
+                        format(userId), verify=False).json()
+    with open("{}.csv".format(userId), 'w', newline='') as csvfile:
+        taskwriter = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        for task in todo:
+            taskwriter.writerow([int(userId), user.get('username'),
+                                 task.get('completed'),
+                                 task.get('title')])
